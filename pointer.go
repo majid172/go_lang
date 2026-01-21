2b2b9396e1d2d8698b045e5c7de63a19181bca72
majid172 <luthfullahhilmajid172@gmail.com>
2026-01-21 15:45:21 +0600

    pointer

--- pointer.go
+++ pointer.go
@@ -10,8 +10,9 @@ func main() {
 	var x int = 10
 	var p  = &x
 	changeValue(p)
-	
+
 	fmt.Println("========")
 	fmt.Println("Address of x:", p,"Value of x using pointer p:", *p)
-	fmt.Println("========")
+	fmt.Println("======pointer example======")
+
 }
