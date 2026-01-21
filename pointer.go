ba596e3b95ecfb9f4e25fcf2f1b79158928a3e39
majid172 <luthfullahhilmajid172@gmail.com>
2026-01-21 11:21:49 +0600

    pointer

---
+++ pointer.go
@@ -0,0 +1,17 @@
+package main 
+import "fmt"
+
+func changeValue(p *int) {
+	*p = 20
+}
+
+func main() {
+	fmt.Println("Pointer Example in Go")
+	var x int = 10
+	var p  = &x
+	changeValue(p)
+	
+	fmt.Println("========")
+	fmt.Println("Address of x:", p,"Value of x using pointer p:", *p)
+	fmt.Println("========")
+}
